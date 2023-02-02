68678c3be7532c1a241d196ca74a2529752e305d
David Myers <myersd33@students.rowan.edu>
2023-02-02 17:20:39 -0500

    added code to initialize green led and toggle between which one blinks

--- 2 Interrupts and Interrupt Routines/Part2.c
+++ 2 Interrupts and Interrupt Routines/Part2.c
@@ -27,6 +27,9 @@ int main(void)
     P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
     P1DIR |= BIT0;                          // Set P1.0 to output direction
 
+    P6OUT &= ~BIT6;                         // clears P6.6 green led
+    P6DIR |= BIT6;                          // sets P6.6 green led as output
+
     // @TODO You need to add in the configuration for the Green LED
 
     P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
@@ -46,9 +49,15 @@ int main(void)
     {
         // @TODO You will need to modify this code to change between blinking the Red LED or the Green LED
         if (ToggleEnable)
+        {
             P1OUT ^= BIT0;                  // P1.0 = toggle
+            P6OUT &= ~BIT6;                 //set P6.6 to 0
+        }
         else
+        {
             P1OUT &= ~BIT0;                 // Set P1.0 to 0
+            P6OUT ^= BIT6;                  // P6.6 = toggle
+        }
         __delay_cycles(100000);
     }
 }
