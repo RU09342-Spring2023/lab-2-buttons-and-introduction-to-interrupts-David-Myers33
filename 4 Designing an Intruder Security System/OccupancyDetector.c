6233a06fc627449b014eae7f00ab75e046a84247
David Myers <myersd33@students.rowan.edu>
2023-02-07 12:25:59 -0500

    Completed pt 4. fixed conditions to determine led status

--- 4 Designing an Intruder Security System/OccupancyDetector.c
+++ 4 Designing an Intruder Security System/OccupancyDetector.c
@@ -5,7 +5,6 @@
  *      Author: David Myers
  */
 #include <msp430.h>
-
 int main(void)
 {
 
@@ -24,24 +23,70 @@ int main(void)
     P4REN |= BIT1;               // Enable Resistor on P4.1
     P4OUT |= BIT1;               // Configure Resistor on P4.1 to be Pullup
 
-    P6OUT^=BIT6;                            // Toggle P6.6 using exclusive-OR
+    P2REN |= BIT3;               //Enable resistor on P2.3
+    P2OUT |= BIT3;               //Configure resistor on P2.3 to be Pullup
 
 
+
+    unsigned int totaltime=0;
     while(1)                                //Loops toggling of LEDs
     {
-        if (!(P4IN & BIT1))         // If S1 (P4.1) is pressed
-        {
-           P1OUT ^= BIT0;          // Toggle P1.0
-           P6OUT &= ~BIT6;
-           __delay_cycles(500000);          //5ms delay to signify warning state
-        }
-        else {
-        P6OUT ^= BIT6;                      // Toggle P6.6 using exclusive-OR
-        P1OUT &= ~BIT0;
-        __delay_cycles(3000000);             // creates a 3s delay for armed status
+        if (((P4IN & BIT1)==0X002) & (totaltime<0x14))// If the left button is not pressed and total count is less than 10 seconds
+              {
+                  P1OUT &= ~BIT0;            // Turns off red led
+                  P6OUT ^= BIT6;             // Makes the Green LED Blink
+                  _delay_cycles(3000000);     // 3 secound delay
+                  totaltime=0;                //resets timer to 0
+              }
+
+
+
+
+          if (((P4IN & BIT1)==0X00) & (totaltime<0x14)) // If the Left Button is pressed and timer is less then 10 secounds
+          {
+
+              P6OUT &= ~BIT6;          // Turns off green led
+              P1OUT ^= BIT0;           // Makes the RED LED Blink
+             _delay_cycles(500000);    // 5ms delay
+             totaltime= totaltime+ 1;  // counts up the timer until it hits 10 seconds (20 cycles *5ms= 10sec)
+          }
+
+
+          if (totaltime==0X14)         // if timer = 10 secounds
+
+           {
+               P1OUT ^= BIT0;          // Makes the RED LED solid
+           }
+
+          if((((P2IN & BIT3)==0X00))) //If the right button is pressed
+                    {
+                       totaltime=0;   // resets the timer
+                    }
+
+        // if (!(P4IN & BIT1))        // If S1 (P4.1) is pressed (inverted)
+        //{
+          //  totaltime= totaltime+ 1;
+            // if (totaltime==0X14)
+           //{
+             //P1OUT &= ~BIT0;
+             //P1OUT |= BIT0; //add code to leave red led on
+           //}
+           //  else if (totaltime!= 0X14)
+            // {
+           //P1OUT ^= BIT0;
+           //P6OUT &= ~BIT6;
+            // }
+       // __delay_cycles(500000);          //5ms delay to signify warning state
+
+       // }
+        //else {                              // if 4.1 is not pressed
+        //P6OUT ^= BIT6;                      // Toggle P6.6 using exclusive-OR
+        //P1OUT &= ~BIT0;
+       // __delay_cycles(3000000);             // creates a 3s delay for armed status
+       // totaltime=0;
         }
     }
-}
+
 
 
 
